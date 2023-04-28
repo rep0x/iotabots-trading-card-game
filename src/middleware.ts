55c3d2812602c05e2c147ee058271085bf4a1df8
Sven Reiser <svreiser@gmail.com>
2023-04-28 16:05:20 +0200

    fixes middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,11 @@
-import { withClerkMiddleware } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next()
-})
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    '/((?!static|.*\\..*|_next|favicon.ico).*)',
-    '/',
-  ],
-}
+  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
+};

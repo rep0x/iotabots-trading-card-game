233c3dc6a5d6234b27bbcf007c2c8d83b5250164
Sven Reiser <svreiser@gmail.com>
2023-04-29 11:58:49 +0200

    fixes card count state for cards.length === 0

--- src/utils/countDeck.ts
+++ src/utils/countDeck.ts
@@ -1,10 +1,13 @@
-import { DeckItemType } from '../types'
+import { DeckItemType } from "../types";
 
 export const countDeck = (cards: DeckItemType[]): number => {
-  let count = 0
-  for (let index = 0; index < cards.length; index += 1) {
-    const element = cards[index]
-    count += element.count
+  let count = 0;
+  if (cards.length === 0) {
+    return count;
   }
-  return count
-}
+  for (let index = 0; index < cards.length; index += 1) {
+    const element = cards[index];
+    count += element.count;
+  }
+  return count;
+};
